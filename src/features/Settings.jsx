aafaefa6edd8b48d7a968261ab65460f3ed41665
Seán Quill <osmosae@gmail.com>
2023-06-17 16:31:54 +0100

    adjusting initial values

--- src/features/Settings.jsx
+++ src/features/Settings.jsx
@@ -6,7 +6,6 @@ function Settings() {
     const settingsInfo = useContext(SettingsContext)
     return (
         <div className="settingsPage">
-            {console.log(typeof settingsInfo.workMinutes)}
             <label>Work: {settingsInfo.workMinutes}:00</label>
             <div className="silderContainer">
                 <input type="range" min={1} max={120} className="slider" defaultValue={settingsInfo.workMinutes} onChange={(event) => settingsInfo.setWorkMinutes(event.target.value)} />
