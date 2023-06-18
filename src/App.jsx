7773575cf64a14f5aff9eacb781810434980bb82
Seán Quill <osmosae@gmail.com>
2023-06-18 18:11:29 +0100

    adding comments

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,20 @@ import Settings from "./features/Settings"
 import SettingsContext from "./features/SettingsContext"
 import Timer from "./features/Timer"
 import { useState } from "react"
+
+/**
+ * App component
+ *
+ * This component renders the main application UI.
+ * It manages the state for showing settings, work minutes,
+ * and break minutes.
+ */
 const App = () => {
+    // State variables
     const [showSettings, setShowSettings] = useState(false)
     const [workMinutes, setWorkMinutes] = useState(30)
     const [breakMinutes, setBreakMinutes] = useState(10)
-
+    // Render the main application UI
     return (
         <main>
             <SettingsContext.Provider
